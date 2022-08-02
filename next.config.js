b1899bfa107b69d789b22a961f9ee1986c56e390
Aman Dhurwey <amandhurwey@Amans-MacBook-Pro.local>
2022-08-02 15:08:29 +0530

    boiler plate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,53 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// sever/next.config.js
+const glob = require("glob");
+const path = require("path");
+
+const pluginNames = glob
+  .sync(path.join(__dirname, "node_modules", "task-leap-v1"))
+  .map((plugin) => plugin.replace(path.join(__dirname, "node_modules"), ""))
+  .map((plugin) => plugin.replace(/\//g, ""));
+
+const withTM = require('next-transpile-modules')(pluginNames);
+require("./plugins"); // prepare plugins
+
+module.exports = withTM({
+  webpack: (config, options) => {
+
+    // // allow compilation of our plugins when we load them from NPM
+    // const rule = config.module.rules[0];
+    // const originalExcludeMethod = rule.exclude;
+    // config.module.rules[0].exclude = (moduleName, ...otherArgs) => {
+    //   // we want to explicitly allow our plugins
+    //   for (const i in pluginNames) {
+    //     if (moduleName.indexOf(`node_modules/${pluginNames[i]}`) >= 0) {
+    //       return false;
+    //     }
+    //   }
+
+    //   // otherwise, use the original rule
+    //   return originalExcludeMethod(moduleName, ...otherArgs);
+    // };
+
+    // // add a rule to compile our plugins from within the monorepo
+    // config.module.rules.push({
+    //   test: /plugins\/.*\.ts?|plugins\/.*.tsx?/,
+    //   use: [options.defaultLoaders.babel],
+    // });
+
+    // we want to ensure that the server project's version of react is used in all cases
+    config.resolve.alias["react"] = path.resolve(
+      __dirname,
+      "node_modules",
+      "react"
+    );
+    config.resolve.alias["react-dom"] = path.resolve(
+      __dirname,
+      "node_modules",
+      "react-dom"
+    );
+
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+})
---
+++ tmp/plugin/task-leap-v1/index.tsx
@@ -0,0 +1,2 @@
+export { default } from "/Users/amandhurwey/Desktop/mac/development/nextjs-shared-plugin/main/main-leap/node_modules/task-leap-v1/pages/index"
+console.info("[Plugin] 'index.tsx' from task-leap-v1");
